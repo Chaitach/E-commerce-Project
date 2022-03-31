5fa09dc7ff58a04c3484f4d4f5ed66852d13c948
Chaitach <94427647+Chaitach@users.noreply.github.com>
2022-03-31 22:37:29 +0700

    Add files via upload

---
+++ client/src/components/mainpages/utils/loading/Loading.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Loading() {
+    return (
+        <div className="load-page">
+            <div className="loader">
+                Loading...
+                    
+            </div>
+        </div>
+    )
+}
+
+export default Loading
